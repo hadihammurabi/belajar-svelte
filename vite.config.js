b64889a5994f3fbf7db694e9a3bf127846a54180
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2022-11-04 15:31:18 +0700

    refactor: code splitting for pages and some libs

--- vite.config.js
+++ vite.config.js
@@ -16,4 +16,31 @@ export default defineConfig({
       ],
     }),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id, { getModuleInfo }) { 
+          const parsed = path.parse(id);
+          const dir = parsed.dir.split('/');
+
+          // code splitting for axios
+          if (dir.includes('axios')) {
+            return 'axios';
+          }
+
+          // code splitting for svelte-query
+          if (dir.includes('@sveltestack') && dir.includes('svelte-query') && parsed.ext == '.svelte') {
+            return 'query';
+          }
+
+          // code splitting for each page
+          if (dir.includes('src') && dir.includes('page') && parsed.ext == '.svelte') {
+            return parsed.name;
+          }
+
+
+        }
+      },
+    },
+  },
 })
